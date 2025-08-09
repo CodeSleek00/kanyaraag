d4c70b629431c388256e3d43f0307d470f1d5743
CodeSleek00 <codesleekstudio@gmail.com>
2025-08-09 22:59:07 +0530

    bas

--- index.php
+++ index.php
@@ -4,18 +4,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>कन्याRaag</title>
-
-  <!-- Google Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
-
-  <!-- Icons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/3.0.0/uicons-thin-rounded/css/uicons-thin-rounded.css'>
-
-  <!-- External CSS -->
   <link rel="stylesheet" href="style.css">
+  <!-- Standard icons -->
+<link rel="icon" type="image/png" sizes="32x32" href="images/कन्या.png">
+<link rel="icon" type="image/png" sizes="16x16" href="images/कन्या.png">
+<link rel="apple-touch-icon" sizes="180x180" href="images/कन्या.png">
+
 </head>
 <body>
 
