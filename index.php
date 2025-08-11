73d308a2971d9f664b44348a9472ef385aeea8be
CodeSleek00 <codesleekstudio@gmail.com>
2025-08-11 23:05:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
 
 <!-- Mobile Footer -->
 <footer class="mobile-footer">
-  <a href="#"><i class="fas fa-home"></i> Home</a>
+  <button class="mobile-menu-btn" id="mobileMenuBtn"><i class="fas fa-bars"></i></button>
   <a href="#"><i class="fas fa-search"></i> Search</a>
   <a href="#"><i class="fas fa-heart"></i> Suggestions</a>
   <a href="#"><i class="fas fa-shopping-bag"></i> Cart</a>
